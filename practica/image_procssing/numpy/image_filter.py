67e35b0088764482e9f008df564fa50910c1e62e
Nils1993 <nilsmoerman1@hotmail.com>
2023-05-16 15:56:14 +0200

    conv_with_pytorch

---
+++ practica/image_procssing/numpy/image_filter.py
@@ -0,0 +1,25 @@
+import io
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+from skimage.measure import block_reduce
+
+class ImageFilter(object):
+    
+    def __init__(self, kernel=None):
+        if kernel is not None:
+            self.imgKernel = kernel
+    
+    def convolve(self, imgTensor):
+        imgTensorRGB = imgTensor.copy() 
+        outputImgRGB = np.empty_like(imgTensorRGB)
+
+        for dim in range(imgTensorRGB.shape[-1]):  # loop over rgb channels
+            outputImgRGB[:, :, dim] = sp.signal.convolve2d (
+                imgTensorRGB[:, :, dim], self.imgKernel, mode="same", boundary="symm"
+            )
+
+        return outputImgRGB
+
+    def downSample(self, img):
+        return block_reduce(image=img, func=np.max)
