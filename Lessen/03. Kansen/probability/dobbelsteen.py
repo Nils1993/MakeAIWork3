5c634b5d44f11cb58c84f45d835da6e782f8a041
Nils1993 <nilsmoerman1@hotmail.com>
2023-05-25 09:25:33 +0200

    kansen

---
+++ Lessen/03. Kansen/probability/dobbelsteen.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import random
+import numpy as np
+
+class Dobbelsteen:
+    
+    def __init__(self):
+        self.values = set(range(1, 7))  
+        self.roll()
+        
+        # https://realpython.com/python-dice-roll
+        self.faces = {
+
+            1: (
+
+                "┌─────────┐\n"
+                "│         │\n"
+                "│    ●    │\n"
+                "│         │\n"
+                "└─────────┘\n"
+            ),
+
+            2: (
+
+                "┌─────────┐\n"
+                "│  ●      │\n"
+                "│         │\n"
+                "│      ●  │\n"
+                "└─────────┘"
+            ),
+
+            3: (
+
+                "┌─────────┐\n"
+                "│  ●      │\n"
+                "│    ●    │\n"
+                "│      ●  │\n"
+                "└─────────┘"
+            ),
+
+            4: (
+
+                "┌─────────┐\n"
+                "│  ●   ●  │\n"
+                "│         │\n"
+                "│  ●   ●  │\n"
+                "└─────────┘"
+
+            ),
+
+            5: (
+
+                "┌─────────┐\n"
+                "│  ●   ●  │\n"
+                "│    ●    │\n"
+                "│  ●   ●  │\n"
+                "└─────────┘"
+            ),
+
+            6: (
+
+                "┌─────────┐\n"
+                "│  ●   ●  │\n"
+                "│  ●   ●  │\n"
+                "│  ●   ●  │\n"
+                "└─────────┘"
+
+            )
+
+        }
+
+    def getList(self):
+        return list(self.values)
+
+    def roll(self):
+        self.number = random.choice(self.getList())
+
+    def getNumber(self):
+        return self.number
+
+    def show(self):        
+        return str(self.faces.get(self.number))
+    
+    def getHistory(self):
+        self.history = random.choice(self.getList())
+        return np.array(self.history)
+
+def main():
+    d = Dobbelsteen()
+    d.roll()
+    d.show()
+
+if __name__ == main():
+    main()                
